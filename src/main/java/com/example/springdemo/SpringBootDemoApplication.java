0111b8f664c18cb2b588bf3f21c2b7a76b6db33a
rajshetye11 <rajshetye11@gmail.com>
2021-09-25 00:08:54 +0530

    [raj] : added SLF4J Logger

--- src/main/java/com/example/springdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springdemo/SpringBootDemoApplication.java
@@ -1,22 +1,25 @@
 package com.example.springdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import com.example.springdemo.component.DemoBean;
 import com.example.springdemo.controller.HelloRestController;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
-
+	public static final Logger logger=LoggerFactory.getLogger(SpringBootDemoApplication.class);
+	
 	public static void main(String[] args) {
-		
-		System.out.println("Welcome to Spring Demo");
-		ApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
-		DemoBean demoBean = context.getBean(DemoBean.class);
-		System.out.println("Demo Bean "+demoBean);
-		System.out.println(context.getBean(HelloRestController.class));
+		logger.info("Hello, Welcome to Spring Demo");
+		ConfigurableApplicationContext context=SpringApplication.run(SpringBootDemoApplication.class, args);
+		DemoBean demoBean=context.getBean(DemoBean.class);
+		logger.debug("Demo Bean= "+demoBean.toString());
+		logger.debug(""+context.getBean(HelloRestController.class));
 	}
 
 }
