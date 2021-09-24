d22f9cdc9672331224c82bba46a95c42d388a530
rajshetye11 <rajshetye11@gmail.com>
2021-09-24 23:48:51 +0530

    [raj] : added ability to display hello world

--- src/main/java/com/example/springdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springdemo/SpringBootDemoApplication.java
@@ -2,12 +2,21 @@ package com.example.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.springdemo.component.DemoBean;
+import com.example.springdemo.controller.HelloRestController;
 
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDemoApplication.class, args);
+		
+		System.out.println("Welcome to Spring Demo");
+		ApplicationContext context = SpringApplication.run(SpringBootDemoApplication.class, args);
+		DemoBean demoBean = context.getBean(DemoBean.class);
+		System.out.println("Demo Bean "+demoBean);
+		System.out.println(context.getBean(HelloRestController.class));
 	}
 
 }
---
+++ src/main/java/com/example/springdemo/controller/HelloRestController.java
@@ -0,0 +1,13 @@
+package com.example.springdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+	@RequestMapping(value = {"","/","/home"} )
+	public String sayHello() {
+		return "Hello from bridgelabz";
+	}
+}
